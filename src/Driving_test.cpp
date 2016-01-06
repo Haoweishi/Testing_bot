39dc9970cd62f3176e73f244fd50afa781957104
James Burnside <21650112@students.avon-schools.org>
2016-01-05 19:04:50 -0500

    upadated
    tada

--- src/Driving_test.cpp
+++ src/Driving_test.cpp
@@ -4,26 +4,59 @@
 class Driving_test{
 public:
 	Driving_test(CANTalon* _leftMot,CANTalon* _rightMot,Joystick* _leftJoy,Joystick* _rightJoy):leftMot(_leftMot),rightMot(_rightMot),leftJoy(_leftJoy),rightJoy(_rightJoy){
-		leftMot->SetPosition(0);
-		rightMot->SetPosition(0);
+		resetPos();
 		printf("Subsystem Driving_test init\n");
+	}
+	void resetPos(){
+		setRightPos(0);
+		setLeftPos(0);
+	}
+	void setRightPos(double d){
+		leftMot->SetPosition(d-leftMot->GetEncVel());
+	}
+	void setLeftPos(double d){
+		rightMot->SetPosition(d-rightMot->GetEncVel());
 	}
 	~Driving_test();
 	void execute(){
 		leftMot->Set(leftJoy->GetY());
 		rightMot->Set(rightJoy->GetY());
-		printf("left vel: %d right vel: %d \n left pos: %d right pos: %d",leftMot->GetEncVel(),rightMot->GetEncVel(),leftMot->GetEncPosition(),rightMot->GetEncPosition());
 	}
-	void moveFeet(double feet){
-		while(leftMot->GetEncPosition()< feet*3.1415*diameter*1000.0){
-				leftMot->Set(.5);
-				rightMot->Set(.5);
+	void moveFeet(double feet=1,double speed=.1){
+		leftMot->SetPosition(0);
+		rightMot->SetPosition(0);
+		printf("moving %f feet", feet);
+		while(abs(leftMot->GetEncPosition())< abs(feet/(3.1415*diameter)*1000.0)){
+			//note on syntax boolean?x:y returns x if boolean is true and y if boolean is false
+			leftMot->Set(feet>0?speed:-speed);rightMot->Set(feet>0?speed:-speed);
 		}
+		printf("done moving straight");
+		leftMot->Set(0);
+		rightMot->SetControlMode(rightMot->kPercentVbus);
+	}
+	//@param theta is in radians, radius is in feet, omega is center speed.
+	void arc(double theta=(3.1415/2),double radius=1,double omega=.1,bool left=true){
+		radius=abs(radius);
+		leftMot->SetPosition(0);
+		rightMot->SetPosition(0);
+		printf("moving to %f feet away curved to %s",radius*sqrt(2),left?"left":"right");
+		const double arcLengthLeft=(radius-left?width:-width/2)*theta;
+		const double arcLengthRight=(radius+left?width:-width/2)*theta;
+		while(left?rightMot->GetEncPosition()<arcLengthRight:leftMot->GetEncPosition()<arcLengthLeft){
+			leftMot->Set(omega*(radius-(left?(width/2):(-width/2))));
+			rightMot->Set(omega*(radius+(left?(width/2):(-width/2))));
+		}
+		leftMot->Set(0);
+		rightMot->Set(0);
+		printf("finished the arc");
 	}
 private:
-	double diameter=.5;
+	//all units are in feet.
+	const double diameter=.5;
 	CANTalon* leftMot;
 	CANTalon* rightMot;
 	Joystick* leftJoy;
 	Joystick* rightJoy;
+	//wheel to wheel width of robot
+	const double width=3.0;
 };
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,9 +2,13 @@
 #include "Joystick.h"
 #include "CANTalon.h"
 #include "Driving_test.cpp"
+#include <String>
+#include "CANSpeedController.h"
+#include "timer.h"
 class Robot: public IterativeRobot
 {
 private:
+	std::string mode;
 	Joystick* leftstick;
 	Joystick* rightstick;
 	CANTalon* leftMotor;
@@ -13,17 +17,30 @@ private:
 	Driving_test* drive;
 	void RobotInit()
 	{
-		lw = LiveWindow::GetInstance();
-		leftstick=new Joystick(1);
-		rightstick=new Joystick(2);
-		leftMotor=new CANTalon(3);
+		printf("testing printf");
 		rightMotor=new CANTalon(4);
+		leftMotor =new CANTalon(3);
+		leftstick =new Joystick(1);
+		rightstick=new Joystick(2);
+		lw = LiveWindow::GetInstance();
 		drive=new Driving_test(leftMotor,rightMotor,leftstick,rightstick);
 	}
-
+	void DisabledInit(){
+		printf("finished %s at %f",mode.c_str(),GetTime());
+		leftMotor->SetControlMode(defaultMode);
+		rightMotor->SetControlMode(defaultMode);
+		leftMotor->Set(0);
+		rightMotor->Set(0);
+	}
 	void AutonomousInit()
 	{
+		printf("AUTO started\n");
+		mode="auto";
 		drive->moveFeet(2);
+		Wait(.5);
+		drive->arc(3.1415,2);
+		Wait(5);
+		printf("AUTO finished\n");
 	}
 
 	void AutonomousPeriodic()
@@ -33,17 +50,20 @@ private:
 
 	void TeleopInit()
 	{
+		mode="Teleop";
 	}
 
 	void TeleopPeriodic()
 	{
 		drive->execute();
+
 	}
 
 	void TestPeriodic()
 	{
 		lw->Run();
 	}
+	const CANSpeedController::ControlMode defaultMode=CANSpeedController::kPercentVbus;
 };
 
 START_ROBOT_CLASS(Robot);

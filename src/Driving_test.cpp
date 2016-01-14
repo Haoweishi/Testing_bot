6001ea0c51e21bf8640bf5e70fb8a846baa587ca
James Burnside <21650112@students.avon-schools.org>
2016-01-14 17:34:53 -0500

    New Update
    Changed update time.

--- src/Driving_test.cpp
+++ src/Driving_test.cpp
@@ -22,20 +22,21 @@ public:
 		leftMot->Set(leftJoy->GetY());
 		rightMot->Set(rightJoy->GetY());
 	}
-	void moveFeet(double feet=1,double speed=.1){
+	void moveFeet(double feet=1,double speed=.5){
 		leftMot->SetPosition(0);
 		rightMot->SetPosition(0);
 		printf("moving %f feet", feet);
 		while(abs(leftMot->GetEncPosition())< abs(feet/(3.1415*diameter)*1000.0)){
 			//note on syntax boolean?x:y returns x if boolean is true and y if boolean is false
-			leftMot->Set(feet>0?speed:-speed);rightMot->Set(feet>0?speed:-speed);
+			leftMot->Set(feet>0?-speed*.85:speed*.85);
+			rightMot->Set(feet>0?speed:-speed);
+			printf("left:%d  right%d \n",abs(leftMot->GetEncPosition()),abs(rightMot->GetEncPosition()));
 		}
 		printf("done moving straight");
-		leftMot->Set(0);
-		rightMot->SetControlMode(rightMot->kPercentVbus);
+		leftMot->Set(0);rightMot->Set(0);
 	}
-	//@param theta is in radians, radius is in feet, omega is center speed.
-	void arc(double theta=(3.1415/2),double radius=1,double omega=.1,bool left=true){
+	//@params theta is in radians, radius is in feet, omega is center speed.
+	void arc(double theta=(3.1415/2),double radius=1,double omega=.5,bool left=true){
 		radius=abs(radius);
 		leftMot->SetPosition(0);
 		rightMot->SetPosition(0);
@@ -43,7 +44,10 @@ public:
 		const double arcLengthLeft=(radius-left?width:-width/2)*theta;
 		const double arcLengthRight=(radius+left?width:-width/2)*theta;
 		while(left?rightMot->GetEncPosition()<arcLengthRight:leftMot->GetEncPosition()<arcLengthLeft){
+			if(leftMot->GetEncVel()==rightMot->GetEncVel()){
 			leftMot->Set(omega*(radius-(left?(width/2):(-width/2))));
+			}else
+				leftMot->Set(0);
 			rightMot->Set(omega*(radius+(left?(width/2):(-width/2))));
 		}
 		leftMot->Set(0);
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -18,8 +18,8 @@ private:
 	void RobotInit()
 	{
 		printf("testing printf");
-		rightMotor=new CANTalon(4);
-		leftMotor =new CANTalon(3);
+		leftMotor=new CANTalon(4,5);
+		rightMotor =new CANTalon(3,5);
 		leftstick =new Joystick(1);
 		rightstick=new Joystick(2);
 		lw = LiveWindow::GetInstance();
@@ -36,9 +36,9 @@ private:
 	{
 		printf("AUTO started\n");
 		mode="auto";
-		drive->moveFeet(2);
+		drive->moveFeet(10);
 		Wait(.5);
-		drive->arc(3.1415,2);
+		//drive->arc(3.1415,2);
 		Wait(5);
 		printf("AUTO finished\n");
 	}
